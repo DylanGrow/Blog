0327dbeaee19c3cf13021accb811062806566bc0
Skylar Grow <dylangrow@gmail.com>
2026-04-20 12:19:22 -0400

    Add sample post data to post.js

--- scripts/post.js
+++ scripts/post.js
@@ -11,3 +11,27 @@ document.getElementById("content").innerHTML = post.content;
 } else {
   document.body.innerHTML = "<h1>Post not found</h1>";
 }
+export const posts = [
+  {
+    id: 1,
+    title: "My First Post",
+    summary: "This is my first blog post.",
+    content: `
+      <p>Welcome to my blog. This is where I document my journey into tech.</p>
+
+      <h2>Why I Started</h2>
+      <p>I wanted a place to track what I'm learning.</p>
+
+      <blockquote>
+        "The best way to learn is to build."
+      </blockquote>
+
+      <h2>Example Code</h2>
+      <pre><code class="language-js">
+function hello() {
+  console.log("Hello world");
+}
+      </code></pre>
+    `
+  }
+];
