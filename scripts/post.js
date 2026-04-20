eea99827a91e559ff58e1091b1b1786eee97b37a
Skylar Grow <dylangrow@gmail.com>
2026-04-20 12:07:57 -0400

    Change content assignment to use innerHTML

--- scripts/post.js
+++ scripts/post.js
@@ -7,7 +7,7 @@ const post = posts.find(p => p.id === id);
 
 if (post) {
   document.getElementById("title").textContent = post.title;
-  document.getElementById("content").textContent = post.content;
+document.getElementById("content").innerHTML = post.content;
 } else {
   document.body.innerHTML = "<h1>Post not found</h1>";
 }
