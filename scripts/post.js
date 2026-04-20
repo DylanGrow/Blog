52289f382917e7ef7e9a15215be83a66f1d231c9
Skylar Grow <dylangrow@gmail.com>
2026-04-20 12:01:36 -0400

    Add post.js to display individual blog posts

---
+++ scripts/post.js
@@ -0,0 +1,13 @@
+import { posts } from "./posts.js";
+
+const params = new URLSearchParams(window.location.search);
+const id = parseInt(params.get("id"));
+
+const post = posts.find(p => p.id === id);
+
+if (post) {
+  document.getElementById("title").textContent = post.title;
+  document.getElementById("content").textContent = post.content;
+} else {
+  document.body.innerHTML = "<h1>Post not found</h1>";
+}
