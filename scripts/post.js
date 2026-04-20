388052025b68553c6e8725e8d8eefff8e95c526f
Skylar Grow <dylangrow@gmail.com>
2026-04-20 18:51:00 -0400

    Refactor post retrieval and display logic

--- scripts/post.js
+++ scripts/post.js
@@ -1,24 +1,13 @@
-export const posts = [
-  {
-    id: 1,
-    title: "My First Post",
-    summary: "This is my first blog post.",
-    content: `
-      <p>Welcome to my blog. This is where I document my journey into tech.</p>
+import { posts } from "./posts.js";
 
-      <h2>Why I Started</h2>
-      <p>I wanted a place to track what I'm learning.</p>
+const params = new URLSearchParams(window.location.search);
+const id = parseInt(params.get("id"));
 
-      <blockquote>
-        "The best way to learn is to build."
-      </blockquote>
+const post = posts.find(p => p.id === id);
 
-      <h2>Example Code</h2>
-      <pre><code class="language-js">
-function hello() {
-  console.log("Hello world");
+if (post) {
+  document.getElementById("title").textContent = post.title;
+  document.getElementById("content").innerHTML = post.content;
+} else {
+  document.body.innerHTML = "<h1>Post not found</h1>";
 }
-      </code></pre>
-    `
-  }
-];
